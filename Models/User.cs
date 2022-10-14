a34a234bda8ab929593f0cd2ff0cb2d26a5d3b0a
Zvonimir Horvat <zvonimirhorvat@Zvonimirs-Air.fritz.box>
2022-10-14 14:37:55 +0200

    Feat: Added CharactedUser db correlation, one to many and updated migrations

--- Models/User.cs
+++ Models/User.cs
@@ -11,5 +11,6 @@ namespace dotnet_rpg.Models
         public string Username { get; set; } = string.Empty;
         public byte[]? PasswordSalt { get; set; }
         public byte[]? PasswordHash { get; set; }
+        public List<Character>? Characters { get; set; }
     }
 }
